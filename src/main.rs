78173457c33639b01df1961fd309ad0a319ae69f
FlareFlo <florianlipp17@gmail.com>
2021-07-29 01:39:00 +0200

    Fuck that weird old bot but this is better

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
 use std::thread::sleep;
 
-use enigo::{Enigo, MouseControllable, MouseButton};
+use inputbot::{KeybdKey::*, MouseButton::*, *};
 use core::time;
 use rand::Rng;
 
 fn main() {
-	let mut enigo = Enigo::new();
 
 	sleep(time::Duration::from_secs(3));
 
-	for height in 1..100 {
-		// for width in 1..100 {
-		// 	enigo.mouse_move_relative(1, 0);
-		// 	sleep(time::Duration::from_millis(1));
-		// }
-		sleep(time::Duration::from_millis(10));
-		enigo.mouse_move_relative(0,1);
+	for height in 1..=500 {
+		for width in 1..500 {
+			MouseCursor::move_rel(1,0);
+		}
+		MouseCursor::move_rel(-500,1);
 	}
 }
