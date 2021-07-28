928dfbf1ef60356b599df91238c8c6b2a2069902
FlareFlo <florianlipp17@gmail.com>
2021-07-29 01:16:19 +0200

    thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::thread::sleep;
+
+use enigo::{Enigo, MouseControllable, MouseButton};
+use core::time;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+	let mut enigo = Enigo::new();
+
+	sleep(time::Duration::from_secs(3));
+
+	let target_y = 10+1;
+	let target_x = 10+1;
+
+	for height in 1..100 {
+		// for width in 1..100 {
+		// 	enigo.mouse_move_relative(1, 0);
+		// 	sleep(time::Duration::from_millis(1));
+		// }
+		sleep(time::Duration::from_millis(10));
+		enigo.mouse_move_relative(0,2);
+	}
 }
