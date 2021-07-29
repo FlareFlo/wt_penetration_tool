2f9982c43cfe129203ebe20cd30ac8f0f5589b10
FlareFlo <florianlipp17@gmail.com>
2021-07-29 15:34:50 +0200

    Capturing and translating mouse location to color

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,44 @@ use inputbot::{KeybdKey::*, MouseButton::*, *};
 use core::time;
 use rand::Rng;
 
+use captrs::*;
+use std::time::Duration;
+use image::GenericImageView;
+use image::ImageBuffer;
+
+
 fn main() {
 
-	sleep(time::Duration::from_secs(3));
+	// sleep(time::Duration::from_secs(3));
+	//
+	// for height in 1..=500 {
+	// 	for width in 1..500 {
+	// 		MouseCursor::move_rel(1,0);
+	// 	}
+	// 	MouseCursor::move_rel(-500,1);
+	// }
+	capture();
+}
+fn capture() {
+	let mut capturer = Capturer::new(0).unwrap();
 
-	for height in 1..=500 {
-		for width in 1..500 {
-			MouseCursor::move_rel(1,0);
+	loop {
+		let ps = capturer.capture_frame().unwrap();
+		let mut new: Vec<Vec<Bgr8>> = Vec::new();
+		for i in 0..1920 {
+			new.push(ps[i..i+1080].to_vec());
 		}
-		MouseCursor::move_rel(-500,1);
+		let x = (MouseCursor::pos().0);
+		let y = (MouseCursor::pos().1);
+		let coords = new[(x + 20) as usize][(y + 20) as usize];
+		println!("R: {} G: {} B: {}", coords.r, coords.g, coords.b);
+		println!("x: {} y: {}", x, y);
+		sleep(Duration::from_millis(1000));
 	}
+}
+
+fn test() {
+	let im = image::open("test.png").unwrap();
+	let first_pixel = im.get_pixel(0, 500);
+	println!("{:?}", first_pixel);
 }
