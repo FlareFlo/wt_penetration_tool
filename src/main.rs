f9e4ebafbf5238d16d767903c0e50600bfb1bc1f
FlareFlo <florianlipp17@gmail.com>
2021-08-09 22:23:26 +0200

    Added screenshot tool

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
-use std::thread::sleep;
-
-use inputbot::{KeybdKey::*, MouseButton::*, *};
 use core::time;
-use rand::Rng;
+use std::thread::sleep;
+use std::time::Duration;
 
 use captrs::*;
-use std::time::Duration;
-use image::GenericImageView;
-use image::ImageBuffer;
+use image::{GenericImage, GenericImageView, ImageBuffer, Rgb, RgbImage};
+use inputbot::{*, KeybdKey::*, MouseButton::*};
+use rand::Rng;
 
+const RED: usize = 0;
+const GREEN: usize = 1;
+const BLUE: usize = 2;
+const ALPHA: usize = 3;
 
 fn main() {
+	// let mut img = image::open("test.png").unwrap();
+	//
+	// let pixel = img.get_pixel(0, 0);
+	// println!("{} {} {} {} ", pixel.0[RED], pixel.0[GREEN], pixel.0[BLUE], pixel.0[ALPHA]);
 
 	// sleep(time::Duration::from_secs(3));
 	//
@@ -20,28 +26,29 @@ fn main() {
 	// 	}
 	// 	MouseCursor::move_rel(-500,1);
 	// }
-	capture();
+
+	screenshot();
 }
-fn capture() {
+
+fn screenshot() {
+	// sleep(Duration::from_millis(3000));
 	let mut capturer = Capturer::new(0).unwrap();
+	sleep(Duration::from_millis(100));
+	let mut ps = capturer.capture_frame().unwrap();
+	println!("Captured");
 
-	loop {
-		let ps = capturer.capture_frame().unwrap();
-		let mut new: Vec<Vec<Bgr8>> = Vec::new();
-		for i in 0..1920 {
-			new.push(ps[i..i+1080].to_vec());
+
+	let mut img: RgbImage = ImageBuffer::new(1920, 1080);
+	for horizontal in 0..1919 {
+		for vertical in 0..1079 {
+			// let coords = new[(horizontal) as usize][(vertical) as usize];
+			// println!("R: {} G: {} B: {}", coords.r, coords.g, coords.b);
+			// println!("x: {} y: {}", horizontal, vertical);
+			img.put_pixel(horizontal, vertical, Rgb::from([ps[(vertical * 1920 + horizontal) as usize].r,ps[(vertical * 1920 + horizontal) as usize].g,ps[(vertical * 1920 + horizontal) as usize].b]));
 		}
-		let x = (MouseCursor::pos().0);
-		let y = (MouseCursor::pos().1);
-		let coords = new[(x + 20) as usize][(y + 20) as usize];
-		println!("R: {} G: {} B: {}", coords.r, coords.g, coords.b);
-		println!("x: {} y: {}", x, y);
-		sleep(Duration::from_millis(1000));
 	}
-}
+	img.save("yeet.png").unwrap();
 
-fn test() {
-	let im = image::open("test.png").unwrap();
-	let first_pixel = im.get_pixel(0, 500);
-	println!("{:?}", first_pixel);
+	// let x = (MouseCursor::pos().0);
+	// let y = (MouseCursor::pos().1);
 }
