2309d7c76f343bdf7d104d09a179c7554c6d1112
FlareFlo <florianlipp17@gmail.com>
2021-08-14 01:06:35 +0200

    Sever optimizations to scanning process

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use core::time;
 use std::thread::sleep;
-use std::time::Duration;
+use std::time::{Duration, Instant};
 
 use captrs::*;
 use image::{GenericImage, GenericImageView, ImageBuffer, Rgb, RgbImage};
@@ -13,10 +13,12 @@ const BLUE: usize = 2;
 const ALPHA: usize = 3;
 
 fn main() {
-	const DIM_X: u32 = 10; // Width
-	const DIM_Y: u32 = 10; //Height
-	const OFF_X: i32 = 1;
-	const OFF_Y: i32 = 1;
+	// Configuration
+	const DIM_X: u32 = 10; 			// Width
+	const DIM_Y: u32 = 10; 			//Height
+	const OFF_X: i32 = 1; 			// Interpolation / pixel density setting (experimental)
+	const OFF_Y: i32 = 1;			// Interpolation / pixel density setting (experimental)
+	const SCREEN_W: u32 = 3840; 	// Screen width
 
 	sleep(time::Duration::from_secs(3));
 
@@ -27,22 +29,30 @@ fn main() {
 	// let pixel = capture_pixel((y) as usize, (x) as usize, 1920 as usize);
 	// println!("{} {} {}", pixel[0], pixel[1], pixel[2]);
 
+	let start = Instant::now();
+
+	let mut capturer = Capturer::new(0).unwrap();
+	sleep(time::Duration::from_millis(50));
+
 	for width in 0..(DIM_Y) {
 		for height in 0..(DIM_X) {
 			MouseCursor::move_rel(OFF_X, 0);
-			let pixel = capture_pixel((height + y) as usize, (width + x) as usize, 1920 as usize);
+			let pixel = capture_pixel((height + y) as usize, (width + x) as usize, SCREEN_W as usize, capturer.capture_frame().unwrap());
 			img.put_pixel(width as u32, height as u32, pixel);
 			// println!("{} {} {}", pixel[0], pixel[1], pixel[2]);
 		}
 		MouseCursor::move_rel(-(DIM_Y as i32), OFF_Y);
 	}
+	let stop = start.elapsed();
+
 	img.save("capture.png").unwrap();
+
+	// Reference time is 23500 ms
+	println!("The program took {}ms and is {}% of the reference timing", stop.as_millis(), stop.as_millis() / 23500 * 100);
 }
 
-fn capture_pixel(y: usize, x: usize, dim_x: usize) -> Rgb<u8> {
-	let mut capturer = Capturer::new(0).unwrap();
-	sleep(time::Duration::from_millis(50));
-	let cap = capturer.capture_frame().unwrap();
+fn capture_pixel(y: usize, x: usize, dim_x: usize, cap: Vec<Bgr8>) -> Rgb<u8> {
+	// let cap = capturer.capture_frame().unwrap();
 	let rgb = Rgb::from([cap[(y * dim_x + x) as usize].r, cap[(y * dim_x + x) as usize].g, cap[(y * dim_x + x) as usize].b]);
 	return rgb;
 }
