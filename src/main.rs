055dc73ae228e001a553e42efda077ec547038eb
FlareFlo <florianlipp17@gmail.com>
2021-08-16 01:41:32 +0200

    Changes by flamion

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,6 @@ fn main() {
 	// const OFF_X: u32 = 1;       // Interpolation / pixel density setting (experimental)
 	// const OFF_Y: u32 = 1;       // Interpolation / pixel density setting (experimental)
 
-	const SQUARE_SIZE_X: u32 = 5; //X Dimensions of the scan square
-	const SQUARE_SIZE_Y: u32 = 5; //Y Dimensions of the scan square
-
-	const NUM_SQUARES_X: u32 = 5;
-	const NUM_SQUARES_Y: u32 = 5;
-
 	// const COOLDOWN: u64 = 10;
 	let screen_width = capturer.width();
 	// let screen_height = capturer.height();
@@ -44,31 +38,36 @@ fn main() {
 	// println!("{} {} {}", pixel[0], pixel[1], pixel[2]);
 
 	let start = Instant::now();
+	let mut skip_counter: u32 = 0;
+
 
 	for square_index_x in 0..NUM_SQUARES_X {
 		for square_index_y in 0..NUM_SQUARES_Y {
 			// let current_position = MouseCursor::pos();
-			let mut corner_pixels = [Rgb::from([0, 0, 0]); 4];
+			let mut corner_pixels = Vec::with_capacity(4); //New vector with the capacity of the pixels we will check
 
 			// waiting 3 frames on average due to mouse-cursor delays
 			let wait = 17 * 3;
 
 			MouseCursor::move_abs((square_index_x * SQUARE_SIZE_X + offset_x) as i32, (square_index_y * SQUARE_SIZE_Y + offset_y) as i32);
-			std::thread::sleep(Duration::from_millis(1));
+			std::thread::sleep(Duration::from_millis(wait));
 			let frame_vec = get_frame(&mut capturer).to_vec();
-			corner_pixels[0] = extract_pixel((square_index_x * SQUARE_SIZE_X) as usize, (square_index_y * SQUARE_SIZE_Y) as usize, offset_x, offset_y, screen_width, &frame_vec);
+			corner_pixels.push(extract_pixel((square_index_x * SQUARE_SIZE_X) as usize, (square_index_y * SQUARE_SIZE_Y) as usize, offset_x, offset_y, screen_width, &frame_vec));
 
 			MouseCursor::move_abs(((square_index_x + 1) * SQUARE_SIZE_X - 1 + offset_x) as i32, (square_index_y * SQUARE_SIZE_Y + offset_y) as i32);
+			std::thread::sleep(Duration::from_millis(wait));
 			let frame_vec = get_frame(&mut capturer).to_vec();
-			corner_pixels[1] = extract_pixel(((square_index_x + 1) * SQUARE_SIZE_X - 1) as usize, (square_index_y * SQUARE_SIZE_Y) as usize, offset_x, offset_y, screen_width, &frame_vec);
+			corner_pixels.push(extract_pixel(((square_index_x + 1) * SQUARE_SIZE_X - 1) as usize, (square_index_y * SQUARE_SIZE_Y) as usize, offset_x, offset_y, screen_width, &frame_vec));
 
 			MouseCursor::move_abs((square_index_x * SQUARE_SIZE_X + offset_x) as i32, ((square_index_y + 1) * SQUARE_SIZE_Y - 1 + offset_y) as i32);
+			std::thread::sleep(Duration::from_millis(wait));
 			let frame_vec = get_frame(&mut capturer).to_vec();
-			corner_pixels[2] = extract_pixel((square_index_x * SQUARE_SIZE_X) as usize, ((square_index_y + 1) * SQUARE_SIZE_Y - 1) as usize, offset_x, offset_y, screen_width, &frame_vec);
+			corner_pixels.push(extract_pixel((square_index_x * SQUARE_SIZE_X) as usize, ((square_index_y + 1) * SQUARE_SIZE_Y - 1) as usize, offset_x, offset_y, screen_width, &frame_vec));
 
 			MouseCursor::move_abs(((square_index_x + 1) * SQUARE_SIZE_X - 1 + offset_x) as i32, ((square_index_y + 1) * SQUARE_SIZE_Y - 1 + offset_y) as i32);
+			std::thread::sleep(Duration::from_millis(wait));
 			let frame_vec = get_frame(&mut capturer).to_vec();
-			corner_pixels[3] = extract_pixel(((square_index_x + 1) * SQUARE_SIZE_X - 1) as usize, ((square_index_y + 1) * SQUARE_SIZE_Y - 1) as usize, offset_x, offset_y, screen_width, &frame_vec);
+			corner_pixels.push(extract_pixel(((square_index_x + 1) * SQUARE_SIZE_X - 1) as usize, ((square_index_y + 1) * SQUARE_SIZE_Y - 1) as usize, offset_x, offset_y, screen_width, &frame_vec));
 
 			//Test for faster image manipulation as the loop below is rather slow
 			// let mut image: dyn GenericImage;
@@ -81,13 +80,14 @@ fn main() {
 			// imga.copy_within(test, 0,0);
 
 			// MouseCursor::move_abs(current_position.0, current_position.1);
-			if all_corners_same(&corner_pixels) {
+			if color_amount_over_threshold_checker(&corner_pixels, 4) {
 				// println!("All corners are the same, filling rectangle!");
 				for pixel_x in 0..SQUARE_SIZE_X {
 					for pixel_y in 0..SQUARE_SIZE_Y {
 						img.put_pixel(square_index_x * SQUARE_SIZE_X + pixel_x, square_index_y * SQUARE_SIZE_Y + pixel_y, corner_pixels[0]);
 					}
 				}
+				skip_counter += 1;
 				continue;
 			}
 
@@ -109,7 +109,7 @@ fn main() {
 	}
 
 	let stop = start.elapsed();
-	println!("Time taken: {}ms", stop.as_millis());
+	println!("Time taken: {}ms, skipped pixel perfect scan {} times", stop.as_millis(), skip_counter);
 
 	img.save("capture.png").unwrap();
 
@@ -131,7 +131,7 @@ fn main() {
 fn get_frame(capturer: &mut Capturer) -> scrap::Frame {
 	loop {
 		if let Ok(frame) = capturer.frame() {
-			//Unsafe due to borrow checker-bug
+			//Unsafe is needed due to borrow checker-bug
 			let frame = unsafe { std::mem::transmute::<_, scrap::Frame<'static>>(frame) };
 			return frame;
 		}
@@ -168,6 +168,6 @@ fn all_corners_same(corner_pixels: &[Rgb<u8>; 4]) -> bool {
 fn extract_pixel(x: usize, y: usize, offset_x: u32, offset_y: u32, display_width: usize, cap: &Vec<u8>) -> Rgb<u8> {
 	let location = ((((y + offset_y as usize) * display_width) + (x + offset_x as usize)) * 4) as usize;
 	let rgb = Rgb::from([cap[location + 2], cap[location + 1], cap[location]]);
-	// println!("Extracting pixel at location x: {} with y: {} offsets are x: {} y: {} delta_x: {} delta_y: {} and got color: {:?}", x, y, offset_x, offset_y, (x + offset_x as usize), (y + offset_y as usize), rgb);
+	//println!("Extracting pixel at location x: {} with y: {} offsets are x: {} y: {} delta_x: {} delta_y: {} and got color: {:?}", x, y, offset_x, offset_y, (x + offset_x as usize), (y + offset_y as usize), rgb);
 	return rgb;
 }
