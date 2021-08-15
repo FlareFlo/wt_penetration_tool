b66888477932d61dbc800d66fda453bfc82e6f4b
FlareFlo <florianlipp17@gmail.com>
2021-08-15 19:49:44 +0200

    Code cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use core::time;
-use std::process::exit;
 use std::thread::sleep;
 use std::time::{Duration, Instant};
 
-use image::{Frame, GenericImage, ImageBuffer, Rgb, RgbImage};
-use image::math::Rect;
-use inputbot::{KeybdKey::EscapeKey, MouseCursor};
+use image::{ImageBuffer, Rgb, RgbImage};
+use inputbot::MouseCursor;
 use scrap::{Capturer, Display};
 
 fn main() {
@@ -13,16 +11,16 @@ fn main() {
 	let mut capturer = Capturer::new(display).expect("Couldn't begin capture.");
 
 	// Configuration
-	const DIM_X: u32 = 10;     // Width
-	const DIM_Y: u32 = 10;     //Height
-	const OFF_X: u32 = 1;       // Interpolation / pixel density setting (experimental)
-	const OFF_Y: u32 = 1;       // Interpolation / pixel density setting (experimental)
+	// const DIM_X: u32 = 10;     // Width
+	// const DIM_Y: u32 = 10;     //Height
+	// const OFF_X: u32 = 1;       // Interpolation / pixel density setting (experimental)
+	// const OFF_Y: u32 = 1;       // Interpolation / pixel density setting (experimental)
 
-	const SQUARE_SIZE_X: u32 = 30; //X Dimensions of the scan square
-	const SQUARE_SIZE_Y: u32 = 30; //Y Dimensions of the scan square
+	const SQUARE_SIZE_X: u32 = 5; //X Dimensions of the scan square
+	const SQUARE_SIZE_Y: u32 = 5; //Y Dimensions of the scan square
 
-	const NUM_SQUARES_X: u32 = 200;
-	const NUM_SQUARES_Y: u32 = 55;
+	const NUM_SQUARES_X: u32 = 5;
+	const NUM_SQUARES_Y: u32 = 5;
 
 	// const COOLDOWN: u64 = 10;
 	let screen_width = capturer.width();
@@ -39,30 +37,10 @@ fn main() {
 
 	let start = Instant::now();
 
-
 	for square_index_x in 0..NUM_SQUARES_X {
 		for square_index_y in 0..NUM_SQUARES_Y {
-			let current_position = MouseCursor::pos();
+			// let current_position = MouseCursor::pos();
 			let mut corner_pixels = [Rgb::from([0, 0, 0]); 4];
-			// I would put it inside a function but right now the borrow checker flags it as multiple mutable borrow inside the loop
-
-
-			//Lets do some fuckery
-			//MouseCursor::move_rel(1, (SQUARE_SIZE_Y - 1) as i32);
-			// let frame_vec = get_frame(&mut capturer).to_vec();
-			// corner_pixels[0] = extract_pixel((square_index_x * SQUARE_SIZE_X) as usize, (square_index_y * SQUARE_SIZE_Y) as usize, offset_x, offset_y, screen_width, &frame_vec);
-			//
-			// MouseCursor::move_rel((SQUARE_SIZE_X - 1) as i32, 0);
-			// let frame_vec = get_frame(&mut capturer).to_vec();
-			// corner_pixels[1] = extract_pixel(((square_index_x + 1) * SQUARE_SIZE_X - 1) as usize, (square_index_y * SQUARE_SIZE_Y) as usize, offset_x, offset_y, screen_width, &frame_vec);
-			//
-			// MouseCursor::move_rel((SQUARE_SIZE_X - 1) as i32 * (-1), (SQUARE_SIZE_Y - 1) as i32 * (-1));
-			// let frame_vec = get_frame(&mut capturer).to_vec();
-			// corner_pixels[2] = extract_pixel((square_index_x * SQUARE_SIZE_X) as usize, ((square_index_y + 1) * SQUARE_SIZE_Y - 1) as usize, offset_x, offset_y, screen_width, &frame_vec);
-			//
-			// MouseCursor::move_rel((SQUARE_SIZE_X - 1) as i32, 0);
-			// let frame_vec = get_frame(&mut capturer).to_vec();
-			// corner_pixels[3] = extract_pixel(((square_index_x + 1) * SQUARE_SIZE_X - 1) as usize, ((square_index_y + 1) * SQUARE_SIZE_Y - 1) as usize, offset_x, offset_y, screen_width, &frame_vec);
 
 
 			MouseCursor::move_abs((square_index_x * SQUARE_SIZE_X + offset_x) as i32, (square_index_y * SQUARE_SIZE_Y + offset_y) as i32);
@@ -92,17 +70,16 @@ fn main() {
 			// };
 			// imga.copy_within(test, 0,0);
 
-			MouseCursor::move_abs(current_position.0, current_position.1);
+			// MouseCursor::move_abs(current_position.0, current_position.1);
 			if all_corners_same(&corner_pixels) {
-				println!("All corners are the same, filling rectangle!");
+				// println!("All corners are the same, filling rectangle!");
 				for pixel_x in 0..SQUARE_SIZE_X {
 					for pixel_y in 0..SQUARE_SIZE_Y {
-						img.put_pixel((square_index_x * SQUARE_SIZE_X + pixel_x), (square_index_y * SQUARE_SIZE_Y + pixel_y), corner_pixels[0]);
+						img.put_pixel(square_index_x * SQUARE_SIZE_X + pixel_x, square_index_y * SQUARE_SIZE_Y + pixel_y, corner_pixels[0]);
 					}
 				}
 				continue;
 			}
-			println!("After rectangle");
 
 
 			for pixel_y in 0..SQUARE_SIZE_Y {
@@ -111,7 +88,7 @@ fn main() {
 					let pos_y = square_index_y * SQUARE_SIZE_Y + pixel_y;
 					MouseCursor::move_abs((pos_x + offset_x) as i32, (pos_y + offset_y) as i32);
 
-					let mut frame;
+					let frame;
 					loop {
 						if let Ok(frame_new) = capturer.frame() {
 							frame = frame_new;
@@ -123,14 +100,12 @@ fn main() {
 					let pixel = extract_pixel(pos_x as usize, pos_y as usize, offset_x, offset_y, screen_width, &frame_vec);
 					img.put_pixel(pos_x, pos_y, pixel);
 				}
-				//MouseCursor::move_abs();
 			}
-			//MouseCursor::move_abs();
 		}
-		//MouseCursor::move_abs();
 	}
 
 	let stop = start.elapsed();
+	println!("Time taken: {}ms", stop.as_millis());
 
 	img.save("capture.png").unwrap();
 
@@ -148,11 +123,11 @@ fn main() {
 	// println!("Debug pixel is {:?}", debug_pixel);
 }
 
-// Deactivated for now, the borrow checker is too stoopid. Gotta copy paste it.
 #[inline(always)]
 fn get_frame(capturer: &mut Capturer) -> scrap::Frame {
 	loop {
 		if let Ok(frame) = capturer.frame() {
+			//Unsafe due to borrow checker-bug
 			let frame = unsafe { std::mem::transmute::<_, scrap::Frame<'static>>(frame) };
 			return frame;
 		}
@@ -168,6 +143,6 @@ fn all_corners_same(corner_pixels: &[Rgb<u8>; 4]) -> bool {
 fn extract_pixel(x: usize, y: usize, offset_x: u32, offset_y: u32, display_width: usize, cap: &Vec<u8>) -> Rgb<u8> {
 	let location = ((((y + offset_y as usize) * display_width) + (x + offset_x as usize)) * 4) as usize;
 	let rgb = Rgb::from([cap[location + 2], cap[location + 1], cap[location]]);
-	println!("Extracting pixel at location x: {} with y: {} offsets are x: {} y: {} delta_x: {} delta_y: {} and got color: {:?}", x, y, offset_x, offset_y, (x + offset_x as usize), (y + offset_y as usize), rgb);
+	// println!("Extracting pixel at location x: {} with y: {} offsets are x: {} y: {} delta_x: {} delta_y: {} and got color: {:?}", x, y, offset_x, offset_y, (x + offset_x as usize), (y + offset_y as usize), rgb);
 	return rgb;
 }
